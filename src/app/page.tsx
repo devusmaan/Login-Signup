9d0e47d2650f416b29753fc2331dac7eda9f987a
devusmaan <devmusmaan@gmail.com>
2024-10-02 13:10:49 +0500

    Revert "All files added"
    This reverts commit a74f412f3231e9b824503772b35d195f0719a3d0.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 "use client"
 
 
-import AllExpenses from "@/components/all-expenses";
 import {auth, signOutUser } from "@/firebase/firebaseauth";
-import { onAuthStateChanged } from "firebase/auth";
-import { useRouter } from "next/navigation";
 
 
 
@@ -12,26 +9,11 @@ import { useRouter } from "next/navigation";
 
 export default function Home() {
 
-  const router = useRouter();
-
-  function expenseBtn() {
-    router.push("/addexpense");
-  }
-
 
 
   return (
     <>
       <h1>Home Page</h1>
-
-      <AllExpenses />
-
-
-      <button onClick={expenseBtn}>Add Expenses</button>
-
-      
-
-      <br /><br /><br />
       <button onClick={() => signOutUser(auth)}>Signout</button>
     </>
   );
